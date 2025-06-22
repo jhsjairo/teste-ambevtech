6b18e46a8bc0dd7a5e63d1050fa37e1b7840dd16
Jairo Alves <jhsjairo@gmail.com>
2025-06-22 15:20:57 -0300

    ajustes .net

--- src/OrderService.Tests/UseCases/CreateOrderUseCaseTests.cs
+++ src/OrderService.Tests/UseCases/CreateOrderUseCaseTests.cs
@@ -1,52 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
-using global::OrderService.Application.DTOs;
-using global::OrderService.Application.Interfaces;
-using global::OrderService.Application.UseCases;
 using Moq;
+using OrderService.Application.DTOs;
+using OrderService.Application.Interfaces;
+using OrderService.Application.UseCases;
 using OrderService.Domain.Entities;
+using OrderService.Domain.Enums;
 using Xunit;
 
 namespace OrderService.Tests.UseCases
 {
-  
-
-
     public class CreateOrderUseCaseTests
     {
         [Fact]
-        public async Task ExecuteAsync_DeveCriarPedidoQuandoNaoExiste()
+        public async Task ExecuteAsync_DeveCriarPedidoEAtualizarCache()
         {
             // Arrange
             var orderRepositoryMock = new Mock<IOrderRepository>();
-            orderRepositoryMock.Setup(x => x.ExistsAsync(It.IsAny<string>()))
-                               .ReturnsAsync(false);
+            var orderCacheMock = new Mock<IOrderCache>();
 
-            var useCase = new CreateOrderUseCase(orderRepositoryMock.Object);
+            orderRepositoryMock.Setup(r => r.ExistsAsync("EX123")).ReturnsAsync(false);
+            orderRepositoryMock.Setup(r => r.GetOrdersByStatusAsync(It.IsAny<OrderStatus>()))
+                               .ReturnsAsync(new List<Order>());
+
+            var useCase = new CreateOrderUseCase(orderRepositoryMock.Object, orderCacheMock.Object);
 
             var dto = new CreateOrderDto
             {
                 ExternalOrderId = "EX123",
                 Items = new List<CreateOrderItemDto>
-            {
-                new() { ProductName = "Produto A", Quantity = 2, UnitPrice = 10.0m },
-                new() { ProductName = "Produto B", Quantity = 1, UnitPrice = 5.5m }
-            }
+                {
+                    new() { ProductName = "Produto A", Quantity = 2, UnitPrice = 10.0m },
+                    new() { ProductName = "Produto B", Quantity = 1, UnitPrice = 5.5m }
+                }
             };
 
             // Act
             await useCase.ExecuteAsync(dto);
 
             // Assert
-            orderRepositoryMock.Verify(r => r.SaveAsync(It.Is<Order>(o =>
-                o.ExternalOrderId == "EX123" &&
-                o.Items.Count == 2 &&
-                o.Total == 25.5m
-            )), Times.Once);
+            orderRepositoryMock.Verify(r => r.SaveAsync(It.IsAny<Order>()), Times.Once);
+            orderCacheMock.Verify(c => c.SetOrderByExternalIdAsync(
+                "EX123", It.Is<Order>(o => o.Total == 25.5m), It.IsAny<TimeSpan>()), Times.Once);
+
+          
         }
 
         [Fact]
@@ -54,18 +53,19 @@ namespace OrderService.Tests.UseCases
         {
             // Arrange
             var orderRepositoryMock = new Mock<IOrderRepository>();
-            orderRepositoryMock.Setup(x => x.ExistsAsync("EX456"))
-                               .ReturnsAsync(true);
+            var orderCacheMock = new Mock<IOrderCache>();
 
-            var useCase = new CreateOrderUseCase(orderRepositoryMock.Object);
+            orderRepositoryMock.Setup(x => x.ExistsAsync("EX456")).ReturnsAsync(true);
+
+            var useCase = new CreateOrderUseCase(orderRepositoryMock.Object, orderCacheMock.Object);
 
             var dto = new CreateOrderDto
             {
                 ExternalOrderId = "EX456",
                 Items = new List<CreateOrderItemDto>
-            {
-                new() { ProductName = "Produto X", Quantity = 1, UnitPrice = 100 }
-            }
+                {
+                    new() { ProductName = "Produto X", Quantity = 1, UnitPrice = 100 }
+                }
             };
 
             // Act
@@ -73,7 +73,7 @@ namespace OrderService.Tests.UseCases
 
             // Assert
             orderRepositoryMock.Verify(r => r.SaveAsync(It.IsAny<Order>()), Times.Never);
+            orderCacheMock.VerifyNoOtherCalls();
         }
     }
-
 }
